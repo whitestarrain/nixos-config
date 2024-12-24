6ccf5c8d2d31ca56a0f92c2b836df6e0f7ec865d
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-12-24 08:10:36 +0800

    [refactor] copy static files to local ranther than fetch from github

--- helper/sources.nix
+++ helper/sources.nix
@@ -1,9 +1,10 @@
 { lib, pkgs, ... }:
 
 {
-  ranger_devicons = builtins.fetchGit {
-    url = "https://github.com/alexanderjeurissen/ranger_devicons.git";
-    rev = "84db73d0a50a8c6085b3ec63f834c781b603e83e";
-  };
+  # ranger_devicons = builtins.fetchGit {
+  #   url = "https://github.com/alexanderjeurissen/ranger_devicons.git";
+  #   rev = "84db73d0a50a8c6085b3ec63f834c781b603e83e";
+  # };
+  ranger_devicons = ./static/ranger_devicons;
 }
 
