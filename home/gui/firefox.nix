68e5ab11b328d5e9d8f78a1b278d40cd5f6b16fe
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-11 22:01:04 +0800

    [feat] firefox add smoothscrool config

--- home/gui/firefox.nix
+++ home/gui/firefox.nix
@@ -18,20 +18,42 @@
       isDefault = true;
       bookmarks = { };
       settings = {
-        "app.update.auto" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.aboutConfig.showWarning" = false;
-
-        "extension.activeThemeID" = "firefox-compact-dark@mozilla.org";
-
+        # common
         "browser.newtabpage.activity-stream.feeds.topsites" = false;
         "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-
         "browser.toolbars.bookmarks.visibility" = "never";
-
-        "extensions.pocket.enabled" = false;
-
         "findBar.highlightAll" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        # about:config
+        "browser.aboutConfig.showWarning" = false;
+        # gfx
+        "gfx.webrender.all" = true;
+        "gfx.webrender.compositor.force-enabled" = true;
+        "gfx.x11-egl.force-enabled" = true;
+        # media
+        "media.av1.enabled" = true;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+        "media.hls.enabled" = true;
+        "media.videocontrols.picture-in-picture.enabled" = false;
+        # security
+        "security.insecure_connection_text.enabled" = true;
+        "security.insecure_connection_text.pbmode.enabled" = true;
+        "security.osclientcerts.autoload" = true;
+        # disable auto update
+        "app.update.auto" = false;
+        "extensions.update.autoUpdateDefault" = false;
+        # default theme
+        "extension.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        # disable pocket
+        "extensions.pocket.enabled" = false;
+        # smooth scrool
+        "general.smoothScroll" = true;
+        "general.smoothScroll.currentVelocityWeighting" = 0;
+        "general.smoothScroll.mouseWheel.durationMaxMS" = 150;
+        "general.smoothScroll.stopDecelerationWeighting" = 0.82;
+        "mousewheel.min_line_scroll_amount" = 10;
+        "general.smoothScroll.msdPhysics.enabled" = true;
       };
     };
   };
