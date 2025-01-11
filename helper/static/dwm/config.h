bb71df28aca00baa7771a7fa4176c42aaef92b6d
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-12 01:18:53 +0800

    [feat] use onedark theme in dwm and st
    - change color theme
    - update dwm border config
    - change layout order

--- helper/static/dwm/config.h
+++ helper/static/dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -13,30 +13,31 @@ static const int swterminheritfs    = 1;        /* 1 terminal inherits fullscree
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 6;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int sidepad            = 15;       /* horizontal padding of bar */
 static const char *fonts[] = {"Sauce Code Pro Nerd Font Mono:size=14",
                               "Source Han Mono SC:size=14",
                               "Symbols Nerd Font Mono:size=14"};
 static const char dmenufont[] = "Sauce Code Pro Nerd Font Mono:size=14";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#dddfff";
-static const char col_gray4[]       = "#ffffff";
-static const char col_gray5[]       = "#805f4e";
-static const char col_cyan[]       = "#000000";
-static const char col_cyan2[]       = "#00ffff";
+static const char col_gray1[]       = "#282c34";
+static const char col_gray2[]       = "#353b45";
+static const char col_gray3[]       = "#3e4451";
+static const char col_white[]       = "#abb2bf";
+static const char col_blue[]       = "#61AFEF";
+static const char col_darkblue[]       = "#355f82";
+static const char col_red[]       = "#e06c75";
+static const char col_yellow[]       = "#e5c07b";
 
-static const unsigned int baralpha    = 140; // 0~255
+static const unsigned int baralpha    = 0xd0; // 0~255
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
-	[SchemeStatus]    = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]   = { "#000000", "#005577",  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]   = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+	[SchemeNorm] = { col_white, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_white, col_gray2,  col_darkblue  },
+	[SchemeStatus]    = { col_white, col_gray1,  col_gray3  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]   = { col_white, col_gray2,  col_gray3  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_white, col_gray1,  col_gray3  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]   = { col_white, col_gray1,  col_gray3  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_white, col_gray1,  col_gray3  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
@@ -78,6 +79,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
@@ -90,7 +92,6 @@ static const Layout layouts[] = {
 	{ ":::",      gaplessgrid },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
@@ -107,7 +108,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_gray2, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
