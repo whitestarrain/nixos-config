443c58b5d9c1f0d6353b637350355deca7c5d5c9
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-11 18:15:49 +0800

    [feat] use interception-tools to swap ctrl and caps

---
+++ modules/linux/desktop/interaction-tools.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  # https://discourse.nixos.org/t/best-way-to-remap-caps-lock-to-esc-with-wayland/39707/6
+  services.interception-tools =
+    let
+      itools = pkgs.interception-tools;
+      itools-caps = pkgs.interception-tools-plugins.caps2esc;
+    in
+    {
+      enable = true;
+      plugins = [ itools-caps ];
+      # requires explicit paths: https://github.com/NixOS/nixpkgs/issues/126681
+      udevmonConfig = pkgs.lib.mkDefault ''
+        - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+    };
+}
+
