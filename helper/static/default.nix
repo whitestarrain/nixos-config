24a66e3452de4240a5f4aa31f9fc8af0a364b9d4
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-12 22:24:01 +0800

    [feat] add wallpaper && set lightdm background && adjust st alpha value

--- helper/static/default.nix
+++ helper/static/default.nix
@@ -6,9 +6,12 @@
   #   rev = "84db73d0a50a8c6085b3ec63f834c781b603e83e";
   # };
   ranger_devicons = ./ranger_devicons;
+
   dwm = ./dwm;
   dmenu = ./dmenu;
   st = ./st;
   dwmblocks = ./dwmblocks;
+
+  wallpaper = ./wallpapers/hello_world.jpg;
 }
 
--- modules/linux/desktop/xserver/lightdm.nix
+++ modules/linux/desktop/xserver/lightdm.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, helper, ... }:
 
 {
   services.xserver.displayManager = {
-    lightdm.enable = true;
+    lightdm = {
+      enable = true;
+      background = lib.mkForce helper.static.wallpaper;
+    };
   };
 }
