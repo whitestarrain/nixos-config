d5fdbd0f7144a379b2d8164636942a9e34d600e2
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-12 19:51:13 +0800

    [feat] patch dwm: truecentertitle

--- helper/static/dwm/dwm.c
+++ helper/static/dwm/dwm.c
@@ -799,7 +799,10 @@ drawbar(Monitor *m)
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeInfoSel : SchemeInfoNorm]);
-			drw_text(drw, x, 0, w - 2 * sp , bh, lrpad / 2, m->sel->name, 0);
+			if (TEXTW(m->sel->name) > w) /* title is bigger than the width of the title rectangle, don't center */
+                drw_text(drw, x, 0, w - 2 * sp , bh, lrpad / 2, m->sel->name, 0);
+			else /* center window title */
+                drw_text(drw, x, 0, w - 2 * sp , bh, (w - TEXTW(m->sel->name)) / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
