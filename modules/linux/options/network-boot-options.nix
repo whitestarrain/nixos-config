496b81ce25a385da033c1befb48317ae70eb7338
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-21 01:41:25 +0800

    networking: split boot options

---
+++ modules/linux/options/network-boot-options.nix
@@ -0,0 +1,63 @@
+{lib, ...}:
+
+{
+  # enable power saving on Intel Wi-Fi
+  boot.extraModprobeConfig = ''
+    options iwlmvm power_scheme=1
+    options iwlwifi power_save=Y power_level=5
+  '';
+
+  # `sysctl -a` or `sysctl -a --pattern ...` to display all values currently available.
+  boot.kernel.sysctl = {
+    # better congestion control algorithm
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+
+    # https://wiki.archlinux.org/title/sysctl
+    "net.ipv4.tcp_fastopen" = 3;
+    "net.ipv4.tcp_max_tw_buckets" = 2000000;
+    "net.ipv4.tcp_max_syn_backlog" = 8192;
+    "net.ipv4.tcp_tw_reuse" = 1;
+    # Set by nix-gaming module
+    # "net.ipv4.tcp_fin_timeout" = 10;
+    "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "net.ipv4.tcp_keepalive_time" = 60;
+    "net.ipv4.tcp_keepalive_intvl" = 10;
+    "net.ipv4.tcp_keepalive_probes" = 6;
+    "net.ipv4.tcp_mtu_probing" = 1;
+    "net.ipv4.tcp_syncookies" = 1;
+
+    "net.ipv4.conf.all.forwarding" = lib.mkForce 1;
+    "net.ipv4.conf.default.forwarding" = lib.mkForce 1;
+    "net.ipv4.conf.*.forwarding" = lib.mkForce 1;
+    "net.ipv6.conf.all.forwarding" = lib.mkForce 1;
+    "net.ipv6.conf.default.forwarding" = lib.mkForce 1;
+    "net.ipv6.conf.*.forwarding" = lib.mkForce 1;
+
+    "net.ipv4.conf.all.rp_filter" = lib.mkForce 0;
+    "net.ipv4.conf.default.rp_filter" = lib.mkForce 0;
+    "net.ipv4.conf.*.rp_filter" = lib.mkForce 0;
+    "net.ipv4.conf.all.accept_redirects" = lib.mkForce 0;
+    "net.ipv4.conf.default.accept_redirects" = lib.mkForce 0;
+    "net.ipv4.conf.*.accept_redirects" = lib.mkForce 0;
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+    "net.ipv4.conf.*.secure_redirects" = 0;
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.send_redirects" = 0;
+    "net.ipv4.conf.*.send_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = lib.mkForce 0;
+    "net.ipv6.conf.default.accept_redirects" = lib.mkForce 0;
+    "net.ipv6.conf.*.accept_redirects" = lib.mkForce 0;
+
+    # Force ARP respond on same interface
+    # https://serverfault.com/questions/834512/why-does-linux-answer-to-arp-on-incorrect-interfaces
+    # But do not set all or *
+    # https://serverfault.com/questions/935366/why-does-arp-ignore-1-break-arp-on-pointopoint-interfaces-kvm-guest
+    "net.ipv4.conf.all.arp_ignore" = 0;
+    "net.ipv4.conf.default.arp_ignore" = 1;
+    "net.ipv4.conf.all.arp_announce" = 0;
+    "net.ipv4.conf.default.arp_announce" = 2;
+  };
+
+}
