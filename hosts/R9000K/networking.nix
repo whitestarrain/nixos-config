416f014eec7ecfb089afd9f02a18c94298862ea5
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-01-20 22:28:03 +0800

    networking: dhcp config

--- hosts/R9000K/networking.nix
+++ hosts/R9000K/networking.nix
@@ -1,14 +1,14 @@
 { lib, helper, ... }:
 
 {
-  imports = (helper.lib.relativeToRootFiles "modules/linux/options" [ "networkd.nix" ]);
+  imports = (helper.lib.relativeToRootFiles "modules/linux/options" [ "networkd.nix" "wireless.nix" ]);
   networking.hostName = "R9000K";
   systemd.network.networks.eno1 = {
     matchConfig.Name = "eno1";
-    DHCP = "true";
+    networkConfig.DHCP = "ipv4";
   };
   systemd.network.networks.wlan0 = {
     matchConfig.Name = "wlan0";
-    DHCP = "true";
+    networkConfig.DHCP = "ipv4";
   };
 }
