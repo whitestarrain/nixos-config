b83136bdb9a5751c40a41e46ef9db26c4c88b77e
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-24 21:09:17 +0800

    general: default disable laptop monitor when using external monitor

--- hosts/R9000K/nvidia.nix
+++ hosts/R9000K/nvidia.nix
@@ -56,7 +56,27 @@
     nvidiaBusId = "PCI:01:0:0";
   };
 
-  # boot.kernelParams = [ "video=DP-2:2560x1440@144" ];
+  # TODO:
+  # disable ForceCompositionPipeline, ForceFullCompositionPipeline declaratively
+  # (nvidia-setting -> X Server Display Configuration -> Advance)
+
+  services.xserver.displayManager.setupCommands = lib.mkAfter ''
+    IN=$(${pkgs.xorg.xrandr}/bin/xrandr | ${pkgs.gnugrep}/bin/grep "eDP" | ${pkgs.gnugrep}/bin/grep " connected" | ${pkgs.gnused}/bin/sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+    EXT=$(${pkgs.xorg.xrandr}/bin/xrandr | ${pkgs.gnugrep}/bin/grep -v "eDP" | ${pkgs.gnugrep}/bin/grep "DP" | ${pkgs.gnugrep}/bin/grep " connected" | ${pkgs.gnused}/bin/sed -e "s/\([A-Z1-9]\+\) connected.*/\1/")
+
+    if (${pkgs.xorg.xrandr}/bin/xrandr | ${pkgs.gnugrep}/bin/grep "$EXT disconnected"); then
+        ${pkgs.xorg.xrandr}/bin/xrandr  --output $EXT --off --output $IN --rate 166 --mode 2560x1600
+    else
+        ${pkgs.xorg.xrandr}/bin/xrandr  --output $IN --off --output $EXT --rate 144 --mode 2560x1440 --scale 1.2x1.2
+    fi
+  '';
+
+  # boot.kernelParams = [
+  #   "video=DP-2:2560x1440@144" # default 60 fps, kernel params can't work
+  #   "video=eDP:d"
+  #   "video=eDP-1:d"
+  #   "video=eDP-2:d"
+  # ];
   # boot.initrd.kernelModules = [ "amdgpu" ];
 
   specialisation = {
