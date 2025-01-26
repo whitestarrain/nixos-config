bfb4563d9a67329b7127eeb6d8df6fe1c4a1e77f
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-26 22:47:01 +0800

    picom: remove xdg autostart

--- modules/linux/desktop/xserver/picom/default.nix
+++ modules/linux/desktop/xserver/picom/default.nix
@@ -3,6 +3,17 @@
 {
   services.picom = {
     enable = true;
+    package = (
+      pkgs.picom.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          postInstall =
+            (previousAttrs.postInstall or "")
+            + ''
+              rm -rf $out/etc/xdg/autostart
+            '';
+        }
+      )
+    );
   };
   systemd.user.services.picom.serviceConfig.ExecStart = lib.mkForce ''
     ${pkgs.picom}/bin/picom --config ${./picom.conf}
