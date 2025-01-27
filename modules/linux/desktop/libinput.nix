c2d32b598e705999a1afe189c84974c3e5e2b8bf
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-27 21:32:42 +0800

    libinput: stop emulating middle click by clicking both right and left buttons'

--- modules/linux/desktop/libinput.nix
+++ modules/linux/desktop/libinput.nix
@@ -1,4 +1,7 @@
 {
   # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
+  services.libinput = {
+    enable = true;
+    mouse.middleEmulation = false;
+  };
 }
