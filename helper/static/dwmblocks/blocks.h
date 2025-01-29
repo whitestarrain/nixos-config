7c020259d1aaf393ca831997ef9acada9237104f
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-29 10:47:40 +0800

    dwmblocks: config dwmblocks for statuscmd

--- helper/static/dwmblocks/blocks.h
+++ helper/static/dwmblocks/blocks.h
@@ -2,15 +2,17 @@
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"","/home/wsain/MyRepo/_temp/click_me.sh", NULL, 0,		1},
-	{" ", NULL,	 get_cpu_usage, 1,		0},
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	 NULL, 2,		0},
-	{"󰂄 ", "if [ -f /sys/class/power_supply/*/capacity ]; then cat /sys/class/power_supply/*/capacity; else echo '--'; fi",	 NULL, 5,		0},
-	{"󰅐 ", "date '+%b %d (%a) %I:%M%p'",			NULL, 30,		0},
+	/*Icon*/	/*Command*/		/*C function*/	/*Update Interval*/	/*Update Signal*/
+	{"", "if [[ $BUTTON -eq  1 ]]; then st-float pulsemixer; fi; echo '[\1 \1'",         NULL,  0,		1},
+	{"", "if [[ $BUTTON -eq  1 ]]; then st-float impala; fi; echo '\2󰖩 \2'",         NULL,  0,		2},
+	{"", "if [[ $BUTTON -eq  1 ]]; then st-float bluetuith; fi; echo '\3󰂯\3]'",         NULL,  0,		3},
+	{"   ", NULL,	 get_cpu_usage, 1,		0},
+	{"   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	 NULL, 2,		0},
+	{"  󰂄 ", "if [ -f /sys/class/power_supply/*/capacity ]; then cat /sys/class/power_supply/*/capacity; else echo '--'; fi",	 NULL, 5,		0},
+	{"  󰅐 ", "date '+%b %d (%a) %I:%M%p'",			NULL, 30,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = "  ";
+static char delim[] = "";
 static unsigned int delimLen = 5;
 
