59a39f9562fb36e85e2f9f204c5a506fa4ebf54b
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-29 10:38:01 +0800

    suckless: patch statuscmd for dwm and dwmblocks

--- helper/static/dwmblocks/blocks.h
+++ helper/static/dwmblocks/blocks.h
@@ -3,6 +3,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"","/home/wsain/MyRepo/_temp/click_me.sh", NULL, 0,		1},
 	{" ", NULL,	 get_cpu_usage, 1,		0},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	 NULL, 2,		0},
 	{"󰂄 ", "if [ -f /sys/class/power_supply/*/capacity ]; then cat /sys/class/power_supply/*/capacity; else echo '--'; fi",	 NULL, 5,		0},
