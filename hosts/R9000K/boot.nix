6386bfe5eaddabc0d45bc98b8f0a5bc3c126c28b
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-04 13:23:43 +0800

    host: add ntfs support

--- hosts/R9000K/boot.nix
+++ hosts/R9000K/boot.nix
@@ -16,6 +16,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
+  # The Linux ntfs implementation only supports read-only, add NTFS support for write on ntfs.
+  # NixOS uses NTFS-3G for NTFS support,
+  # Usage: mount -t ntfs-3g /dev/sda1 /mnt/sda1
+  boot.supportedFilesystems = [ "ntfs" ];
+
   # boot animation
   # boot.plymouth = {
   #   enable = true;
