6281f05cbac68b6a7c5759426ec103e13341ea2e
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-05 12:23:45 +0800

    general: add sshfs support

--- modules/linux/base/ssh.nix
+++ modules/linux/base/ssh.nix
@@ -1,7 +1,14 @@
+{ pkgs, ... }:
+
 {
+  environment.systemPackages = with pkgs; [
+    sshfs
+  ];
+
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
+    allowSFTP = true;
     settings = {
       PasswordAuthentication = false; # disable password login
     };
