fde66755a7cc20d6679288bb6b856f6cd9efb316
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-05 12:01:26 +0800

    suckless: config firefox tagmask

--- helper/static/dwm/config.h
+++ helper/static/dwm/config.h
@@ -63,7 +63,7 @@ static const Rule rules[] = {
 	 */
 	/* class            instance    title            tags mask  iscentered  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",           NULL,       NULL,            0,         0,          1,          0,           0,        -1 },
-	{ "Firefox",        NULL,       NULL,            0,         0,          0,          0,          -1,        -1 },
+	{ "firefox",        NULL,       NULL,            1<<1,      0,          0,          0,          -1,        -1 },
 	{ "St",             NULL,       NULL,            0,         0,          0,          1,           0,        -1 },
 	{ "st-256color",    NULL,       NULL,            0,         0,          0,          1,           0,        -1 },
 	{ "st-float",       NULL,       NULL,            0,         1,          1,          1,           0,        -1 },
