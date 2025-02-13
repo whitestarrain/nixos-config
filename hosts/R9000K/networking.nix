0d943ca097e1dd063869809e7d79e3a7eb1f30bc
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-14 00:06:57 +0800

    host: prioritize using wired network

--- hosts/R9000K/networking.nix
+++ hosts/R9000K/networking.nix
@@ -12,6 +12,10 @@
   systemd.network.networks.eno1 = {
     matchConfig.Name = "eno1";
     networkConfig.DHCP = "ipv4";
+    dhcpV4Config = {
+      # default 1024
+      RouteMetric = 512;
+    };
   };
   # `sudo rfkill unblock all` to unlock wlan0
   systemd.network.networks.wlan0 = {
