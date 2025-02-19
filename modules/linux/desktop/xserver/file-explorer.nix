afa9ee31714856016788fd384e8349d654ddb8e4
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-19 23:55:24 +0800

    thunar: delete thunar service

--- modules/linux/desktop/xserver/file-explorer.nix
+++ modules/linux/desktop/xserver/file-explorer.nix
@@ -1,19 +1,31 @@
 { pkgs, ... }:
 
+let
+  # When firefox opens a folder, thunar.service will be automatically started,
+  # but thunar can't correctly use mime types to open files.
+  package = (pkgs.xfce.thunar.override {
+    thunarPlugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  }).overrideAttrs (old: {
+    # Derivation of thunar with plugins is defined by pkgs.symlinkJoin,
+    # use buildCommand to override.
+    buildCommand =
+      (old.buildCommand or "")
+      + ''
+        rm -rf $out/share/systemd/user
+        rm -rf $out/lib/systemd/user
+      '';
+  });
+in
 {
   services = {
     tumbler.enable = true; # Thumbnail support for images
     gvfs.enable = true; # Mount, trash, and other functionalities
   };
-
-  programs = {
-    # thunar file manager(part of xfce) related options
-    thunar = {
-      enable = true;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-      ];
-    };
-  };
+  programs.xfconf.enable = true;
+  environment.systemPackages = [
+    package
+  ];
 }
