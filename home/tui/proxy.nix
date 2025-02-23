846ebb3ec00d0f47cb9a87ef396c82bcb1549865
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-02-23 10:47:08 +0800

    proxy: add switchproxy command

--- home/tui/proxy.nix
+++ home/tui/proxy.nix
@@ -2,29 +2,41 @@
 
 let
   clash-verge-pkg = pkgs.clash-verge-rev;
+  switchproxy = pkgs.writeShellApplication {
+    name = "switchproxy";
+    text = ''
+      default_proxy="http://127.0.0.1:7890"
+
+      # run with proxy
+      if [ $# -gt 0 ]; then
+        export http_proxy="$default_proxy"
+        export https_proxy="$default_proxy"
+        export all_proxy="$default_proxy"
+        "$@"
+        exit
+      fi
+
+      # switch proxy
+      if [ -n "$http_proxy" ] || [ -n "$https_proxy" ] || [ -n "$all_proxy" ]; then
+        echo "unset terminal proxy"
+        unset http_proxy https_proxy all_proxy
+        exit
+      fi
+      export http_proxy="$default_proxy"
+      export https_proxy="$default_proxy"
+      export all_proxy="$default_proxy"
+      echo "set terminal proxy"
+    '';
+  };
 in
 {
   home.packages = [
     pkgs.proxychains-ng
+    switchproxy
   ];
 
-  programs.bash = {
-    initExtra = ''
-      # swich proxy
-      alias sp="switchproxy"
-      function switchproxy {
-        if [ -n "$http_proxy" ] || [ -n "$https_proxy" ] || [ -n "$all_proxy" ]; then
-          echo "unset terminal proxy"
-          unset http_proxy https_proxy all_proxy
-          return
-        fi
-        default_proxy="http://127.0.0.1:7890"
-        export http_proxy="$default_proxy"
-        export https_proxy="$default_proxy"
-        export all_proxy="$default_proxy"
-        echo "set terminal proxy"
-      }
-    '';
+  programs.bash.shellAliases = {
+    sp = "switchproxy";
   };
 }
 
