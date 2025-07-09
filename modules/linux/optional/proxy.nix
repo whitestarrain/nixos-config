9bd8aa318da47024f18b8186779b5ee850884eaf
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-07-10 00:16:49 +0800

    proxy: run v2raya with root

--- modules/linux/optional/proxy.nix
+++ modules/linux/optional/proxy.nix
@@ -1,36 +1,7 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}:
+{ pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.v2raya
-  ];
-  systemd.user.services.v2raya = {
+  services.v2raya = {
     enable = true;
-    unitConfig = {
-      Description = "v2rayA service";
-      After = [
-        "network.target"
-        "nss-lookup.target"
-        "network-online.target"
-        "iptables.service"
-        "ip6tables.service"
-        "nftables.service"
-      ];
-      Wants = [ "network.target" ];
-    };
-    serviceConfig = {
-      ExecStartPre="${pkgs.coreutils}/bin/sleep 10";
-      ExecStart = "${lib.getExe pkgs.v2raya} --lite --log-disable-timestamp --address 127.0.0.1:2017 --log-file /dev/null";
-      Restart = "on-failure";
-      RestartSec = 5;
-      Type = "simple";
-    };
-    # InstallConfig
-    wantedBy = [ "default.target" ];
   };
 }
