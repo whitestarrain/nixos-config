d49f118e9c0716e8307771a5b4b040f85a926ffc
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-07-17 23:31:17 +0800

    clipboard: add alias command

--- home/gui/clipboard.nix
+++ home/gui/clipboard.nix
@@ -1,15 +1,25 @@
 {
   pkgs,
   lib,
+  config,
   helper,
   ...
 }:
 
+let
+  copyq = helper.lib.wrapEnv pkgs.copyq "bin/copyq" {
+    QT_SCALE_FACTOR = 1.5;
+  };
+  backupScript = pkgs.writeShellScriptBin "clipmenu" ''
+    ${copyq}/bin/copyq menu
+  '';
+in
 {
   services.copyq = {
     enable = true;
-    package = helper.lib.wrapEnv pkgs.copyq "bin/copyq" {
-      QT_SCALE_FACTOR = 1.5;
-    };
+    package = copyq;
   };
+  home.packages = [
+    backupScript
+  ];
 }
