b3d307996cd0657d81f26910007b5b23eb9676e8
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-07-26 21:35:24 +0800

    general: add gammastep service to protect eyes

---
+++ home/gui/eye-protection.nix
@@ -0,0 +1,26 @@
+{ ... }:
+{
+  services.gammastep = {
+    enable = true;
+    tray = false;
+    # test: gammastep -x && sleep 1 && gammastep -O 5500
+    temperature = {
+      day = 5500;
+      night = 4700;
+    };
+    settings = {
+      general = {
+        fade = "1";
+        brightness-day = "1.0";
+        brightness-night = "0.8";
+        location-provider = "manual";
+        adjustment-method = "randr"; # for xorg
+      };
+      manual = {
+        # China Bejing
+        lat = "39.9"; # latitude
+        lon = "116.3"; # longitude
+      };
+    };
+  };
+}
