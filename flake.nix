91d58ab871818aeecec8b7eb1dea75a54b2d6af1
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-12-08 01:19:57 +0800

    general: update to nixos 25.11, update lanzaboote

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     };
     # secure boot
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
